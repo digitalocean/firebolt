34a8a56905173ea392f75eccf73732639e6dd3e0
leem <leem@starktechgroup.com>
2023-01-10 06:08:48 -0500

    moved metrics to yml object and test config to testdata dir

--- examples/main.go
+++ examples/main.go
@@ -18,7 +18,7 @@ import (
 
 // choose an example to run; see 'runKafkaToKafka()' or 'runLogging()' for the actual example code
 func main() {
-	exampleName := os.Args[1]
+	exampleName := "kafkatokafka"
 	testutil.WaitForPort(nil, 9092) // wait for kafka to be ready
 	testutil.WaitForPort(nil, 9200) // wait for elasticsearch to be ready
 
