a0a454a086fa85d0a30eeb723372bfd60f4aa9d8
Michael Fuller <mfuller@digitalocean.com>
2023-03-23 16:21:43 -0500

    node/elasticsearch/metrics_test.go: test that n buckets exist, ignoring values. also, stop testing behaviors specific to prometheus, as that is not firebolts concern

--- node/elasticsearch/metrics_test.go
+++ node/elasticsearch/metrics_test.go
@@ -15,18 +15,8 @@ func Test_generateElasticsearchProcessTimeBuckets(t *testing.T) {
 	tests := []struct {
 		name      string
 		args      args
-		want      []float64
-		wantPanic bool
+		wantCount int
 	}{
-		{
-			name: "panics upon bad input",
-			args: args{
-				min:   -0.01,
-				max:   0.00,
-				count: 0,
-			},
-			wantPanic: true,
-		},
 		{
 			name: "creates expected number of default buckets",
 			args: args{
@@ -34,19 +24,12 @@ func Test_generateElasticsearchProcessTimeBuckets(t *testing.T) {
 				max:   10,
 				count: 8,
 			},
-			want:      []float64{0.01, 0.026826957952797256, 0.07196856730011518, 0.19306977288832497, 0.5179474679231209, 1.3894954943731366, 3.7275937203149367, 9.999999999999991},
-			wantPanic: false,
+			wantCount: 8,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.wantPanic {
-				assert.Panics(t, func() {
-					generateElasticsearchProcessTimeBuckets(tt.args.min, tt.args.max, tt.args.count)
-				}, nil)
-				return
-			}
-			assert.Equalf(t, tt.want, generateElasticsearchProcessTimeBuckets(tt.args.min, tt.args.max, tt.args.count), "generateElasticsearchProcessTimeBuckets(%v, %v, %v)", tt.args.min, tt.args.max, tt.args.count)
+			assert.Equalf(t, tt.wantCount, len(generateElasticsearchProcessTimeBuckets(tt.args.min, tt.args.max, tt.args.count)), "generateElasticsearchProcessTimeBuckets(%v, %v, %v)", tt.args.min, tt.args.max, tt.args.count)
 		})
 	}
 }
